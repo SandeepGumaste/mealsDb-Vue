fad4465dc4e349155831f23183e1b6da7362ebae
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2023-12-04 11:30:19 +0530

    views wip

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,37 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow-md h-16 flex justify-between">
+      <RouterLink
+        :to="{ name: 'home' }"
+        class="inline-flex items-center h-full px-5"
+      >
+        Home
+      </RouterLink>
+      <div class="flex items-center gap-1">
+        <RouterLink
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+        >
+          Search Meals
+        </RouterLink>
+        <RouterLink
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+        >
+          Meals by Letter
+        </RouterLink>
+        <RouterLink
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+        >
+          Meals by Ingredients
+        </RouterLink>
+      </div>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
